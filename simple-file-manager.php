d8efd5545a577da97eff3e16116f6ab7fbd32c44
Harri Heljala <harri.heljala@gmail.com>
2016-03-09 01:06:54 +0200

    Unifying commenting and variable naming.

--- simple-file-manager.php
+++ simple-file-manager.php
@@ -5,9 +5,9 @@ class Simple_File_Manager {
 	/**
 	* Creates a zip file from a file or a folder recursively without a full nested folder structure inside the zip file
 	* Based on: http://stackoverflow.com/a/1334949/3073849
-	* @param $source			The path of the folder you want to zip
-	* @param $destination		The path of the zip file you want to create
-	* @return Returns TRUE on success or FALSE on failure.
+	* @param      string   $source      The path of the folder you want to zip
+	* @param      string   $destination The path of the zip file you want to create
+	* @return     bool     Returns TRUE on success or FALSE on failure.
 	*/
 	public static function zip( $source, $destination ) {
 
@@ -27,17 +27,17 @@ class Simple_File_Manager {
 
 			foreach ( new RecursiveIteratorIterator(
 				new RecursiveDirectoryIterator( $source, FilesystemIterator::SKIP_DOTS ),
-				RecursiveIteratorIterator::SELF_FIRST ) as $file ) {
+				RecursiveIteratorIterator::SELF_FIRST ) as $path ) {
 
-				$file = str_replace( '\\', '/', $file );
-				$file = realpath( $file );
+				$path = str_replace( '\\', '/', $path );
+				$path = realpath( $path );
 
-				if ( is_dir( $file ) ) {
-					$zip->addEmptyDir( str_replace( $source . '/', '', $file . '/' ) );
+				if ( is_dir( $path ) ) {
+					$zip->addEmptyDir( str_replace( $source . '/', '', $path . '/' ) );
 				}
 
-				elseif ( is_file( $file ) ) {
-					$zip->addFile( $file, str_replace( $source . '/', '', $file ) );
+				elseif ( is_file( $path ) ) {
+					$zip->addFile( $path, str_replace( $source . '/', '', $path ) );
 				}
 			}
 		}
@@ -51,10 +51,10 @@ class Simple_File_Manager {
 	
 	/**
 	 * Extracts a zip file to given folder. Overwrite deletes an existing destination folder and replaces it with the content of the zip file.
-	 * @param $source 			The path of the zip file you want to extract
-	 * @param $destination 		The path of the folder you want to extract to
-	 * @param $overwrite 		Whether to overwrite an existing destination folder
-	 * @return Returns TRUE on success or FALSE on failure.
+	 * @param       string   $source      The path of the zip file you want to extract
+	 * @param       string   $destination The path of the folder you want to extract to
+	 * @param       bool     $overwrite   Whether to overwrite an existing destination folder
+	 * @return      bool     Returns TRUE on success or FALSE on failure.
 	 **/
 	public static function unzip( $source, $destination, $overwrite = false ) {
 
@@ -99,8 +99,8 @@ class Simple_File_Manager {
 	/**
 	 * Delete a file, or recursively delete a folder and it's contents
 	 * Based on: http://stackoverflow.com/a/15111679/3073849
-	 * @param $source 			The path of the file or folder
-	 * @return Returns TRUE on success or if file already deleted or FALSE on failure.
+	 * @param       string   $source The path of the file or folder
+	 * @return      bool     Returns TRUE on success or if file already deleted or FALSE on failure.
 	 **/
 	public static function delete( $source ) {
 
@@ -134,8 +134,8 @@ class Simple_File_Manager {
 	/**
 	 * Copy a file, or recursively copy a folder and its contents
 	 * Based on: http://stackoverflow.com/a/12763962/3073849
-	 * @param       string   $source    Source path
-	 * @param       string   $destination      Destination path
+	 * @param       string   $source      Source path
+	 * @param       string   $destination Destination path
 	 * @param       string   $permissions New folder creation permissions
 	 * @return      bool     Returns true on success, false on failure
 	 **/
@@ -177,18 +177,18 @@ class Simple_File_Manager {
 
 	/**
 	 * Creates a folder recursively.
-	 * @param  		string 	$path The path of the folder to create
-	 * @param  		int 	$mode The mode given for the folder. The default mode (0764) used by the method is less permissive than the php default of (0777).
+	 * @param  		string 	$path        The path of the folder to create
+	 * @param  		int 	$permissions The mode given for the folder. The default mode (0764) used by the method is less permissive than the php default of (0777).
 	 * @return      bool    Returns true if the folder already existed or if it was created on successfully, false on failure.
 	 */
-	public static function mkdir( $path, $mode = 0764 ) {
+	public static function mkdir( $path, $permissions = 0764 ) {
 
 		// Folder exists already, return true
 		if ( file_exists( $path ) ) {
 			return true;
 		}
 
-		return mkdir( $path, $mode, true );
+		return mkdir( $path, $permissions, true );
 
 	}
 
